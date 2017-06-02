59a92533e345a80b989324378364b0d99323c2e2
Jose Camino <jcamino86@gmail.com>
2017-06-02 15:33:52 -0400

    Carousels are now swipe enabled

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,1 +1,20 @@
-$('.carousel').carousel()
+$('.carousel').carousel();
+
+$(document).ready(function() {  
+    
+    // Using TouchSwipe plugin to enable carousel swiping
+    // Code snippet from: https://lazcreative.com/blog/adding-swipe-support-to-bootstrap-carousel-3-0
+
+    //Enable swiping...
+    $(".carousel-inner").swipe( {
+    	//Generic swipe handler for all directions
+    	swipeLeft:function(event, direction, distance, duration, fingerCount) {
+    		$(this).parent().carousel('next'); 
+    	},
+    	swipeRight: function() {
+    		$(this).parent().carousel('prev'); 
+    	},
+    	//Default is 75px, set to 0 for demo so any distance triggers swipe
+    	threshold:0
+    });
+});
